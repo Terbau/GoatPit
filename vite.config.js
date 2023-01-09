bfc867cd8bf39832b6e5277737151ce595c5830c
Terbau <bragebau@hotmail.no>
2023-01-09 01:13:14 +0100

    Fix dev server CORS issues

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	build: { commonjsOptions: { transformMixedEsModules: true, exclude: [ 'node_modules/lodash-es/**', 'node_modules/@types/lodash-es/**', ] } },
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '127.0.0.1',
+		// port: 6137
+	}
 };
 
 export default config;
